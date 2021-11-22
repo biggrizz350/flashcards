1e6946faaf62316ef82362e71372ec826059e7b0
Robert Witters <41020734+biggrizz350@users.noreply.github.com>
2021-11-22 12:58:31 -0500

    Flash card style update

--- src/Flashcard.js
+++ src/Flashcard.js
@@ -5,14 +5,15 @@ import React, { useState } from 'react'
 
 export default function Flashcard({ flashcard }) { // the argument is being destructed as it comes in
     const [flip, setFlip] = useState(false)
+
     return (
         <div
-        className={`card ${flip ? 'flip' : ''}`} // static class of card, utilizing the flip variable for tenerary operation
+        className={` card ${flip ? 'flip' : ''}`} // static class of card, utilizing the flip variable for tenerary operation
         onClick={()=> setFlip(!flip)}
         >
             <div className="front">
                 {flashcard.question}
-                <div className='flashcard-options'>
+                <div className="flashcard-options">
                     {flashcard.options.map(option => {
                         return <div className="flashcard-option">{option}</div> //Loops through each option, option for each
                     })}
